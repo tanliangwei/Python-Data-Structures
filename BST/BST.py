e5630fc21935951ad1229ea670c68176fcb48caf
Liang Wei Tan Student <tan_liang_wei@berkeley.edu>
2019-06-13 07:40:32 -0700

    added insert functionality

--- BST/BST.py
+++ BST/BST.py
@@ -5,11 +5,35 @@ This file contains the BST data structure. It consist of 2 classes
 """
 
 class Node:
-	def __init__(self, left_child=None, right_child=None, parent=None, key=None):
-		self.left_child=left_child
-		self.right_child=right_child
+	def __init__(self, key, left=None, right=None, parent=None):
+		self.left=left
+		self.right=right
 		self.parent=parent
 		self.key=key
 
+class BST:
+	def __init__(self, root=None):
+		self.root=root
 
-node =Node()
+	def insert(self, key, node=None):
+		temp_node=node
+		if node is None:
+			temp_node=self.root
+		if temp_node is None:
+			self.root=Node(key=key)
+		elif key<=temp_node.key:
+			if temp_node.left is None:
+				temp_node.left=Node(key=key, parent=temp_node)
+			else:
+				self.insert(key, temp_node.left)			
+		elif key>temp_node.key:
+			if temp_node.right is None:
+				temp_node.right=Node(key=key, parent=temp_node)
+			else:
+				self.insert(key, temp_node.right)
+
+
+
+# Node test 
+
+# Insert Test
