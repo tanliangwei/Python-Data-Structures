43d1d4372b5dbffe813f12ddbc97864c3d1e7083
Liang Wei Tan Student <tan_liang_wei@berkeley.edu>
2019-06-16 09:52:00 -0700

    implemented successful factory pattern usage

--- BST/BST_test.py
+++ BST/BST_test.py
@@ -15,8 +15,14 @@ class Count_BST(BST):
 		if not reverse:
 			for i in range(len(list_of_nodes)-1, -1, -1):
 				node = list_of_nodes[i]
-				number_of_left = node.left or 0
-				number_of_right = node.right or 0
+				number_of_left = node.left.count if node.left is not None else 0
+				number_of_right = node.right.count if node.right is not None else 0
+				node.count=number_of_right+number_of_left+1
+		if reverse:
+			for i in range(0, len(list_of_nodes)):
+				node = list_of_nodes[i]
+				number_of_left = node.left.count if node.left is not None else 0
+				number_of_right = node.right.count if node.right is not None else 0
 				node.count=number_of_right+number_of_left+1
 
 
@@ -28,3 +34,17 @@ class Count_BST(BST):
 if __name__ == '__main__':
 	count_bst = Count_BST()
 	print(count_bst.Node)
+	count_bst = BST()
+	count_bst.insert(10)
+	count_bst.insert(15)
+	count_bst.insert(100)
+	count_bst.insert(750)
+	count_bst.insert(75)
+	count_bst.insert(175)
+	# count_bst.delete(175)
+	print(count_bst.delete(75))
+	print(count_bst.find(100))
+	print(count_bst.get_smaller_than(10))
+	print(count_bst.get_smaller_than(15))
+	print(count_bst.get_smaller_than(750))
+	print(count_bst.get_smaller(100))
