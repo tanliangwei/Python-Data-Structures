57757f44b67772ebc64bb86a9f395476e7fc91ea
Liang Wei Tan Student <tan_liang_wei@berkeley.edu>
2019-06-15 17:49:02 -0700

    implemented strict versions of inequality for BST code

--- BST/BST.py
+++ BST/BST.py
@@ -93,30 +93,50 @@ class BST:
 				return None
 			return self.find_node(key, temp_node.right)
 
-	def get_larger(self, key):
-		node = self.find_node(key)
+	def get_larger(self, key, node=None):
+		temp_node=node
 		if node is None:
+			temp_node=self.root
+		if self.root is None:
 			return None
-		if node.right is not None:
-			return self.get_min_node(node.right).key
-		while node.parent is not None:
-			if node.parent.right is node:
-				node = node.parent
+		select_node = self.find_node(key, temp_node)
+		if select_node is None:
+			return None
+		if select_node.right is not None:
+			min_node = self.get_min_node(select_node.right)
+			if min_node.key>key:
+				return min_node.key
+			return self.get_larger(key, min_node)
+		while select_node.parent is not None:
+			if select_node.parent.right is select_node:
+				select_node = select_node.parent
 			else:
-				return node.parent.key
+				if select_node.parent.key>key:
+					return select_node.parent.key
+				return self.get_larger(key, select_node.parent)
 		return None
 
-	def get_smaller(self, key):
-		node = self.find_node(key)
+	def get_smaller(self, key, node=None):
+		temp_node=node
 		if node is None:
+			temp_node=self.root
+		if self.root is None:
 			return None
-		if node.left is not None:
-			return self.get_max_node(node.left).key
-		while node.parent is not None:
-			if node.parent.left is node:
-				node = node.parent
+		select_node = self.find_node(key, temp_node)
+		if select_node is None:
+			return None
+		if select_node.left is not None:
+			max_node = self.get_max_node(select_node.left)
+			if max_node.key<key:
+				return max_node.key
+			return self.get_smaller(key, max_node)
+		while select_node.parent is not None:
+			if select_node.parent.left is select_node:
+				select_node = select_node.parent
 			else:
-				return node.parent.key
+				if select_node.parent.key<key:
+					return select_node.parent.key
+				return self.get_smaller(key, select_node.parent)
 		return None
 
 	def get_larger_than(self, key):
@@ -154,6 +174,9 @@ class BST:
 
 bst = BST()
 bst.insert(10)
+bst.insert(10)
+bst.insert(15)
+bst.insert(15)
 bst.insert(100)
 bst.insert(750)
 bst.insert(75)
@@ -162,7 +185,7 @@ print(bst.find(175))
 print(bst.find(176))
 print(bst.get_smaller_than(10))
 print(bst.get_smaller_than(15))
-print(bst.find(15))
+# print(bst.find(15))
 print(bst.get_smaller_than(750))
 print(bst.get_smaller_than(100))
 
