7af01dadba2c4b3ca98dd49b48ef40d7fcddeedf
Liang Wei Tan Student <tan_liang_wei@berkeley.edu>
2019-07-13 18:21:16 -0700

    cycle detector implemented using better dfs

--- GraphSearch/dfs.py
+++ GraphSearch/dfs.py
@@ -7,7 +7,7 @@ sys.path.append("/Users/tanliangwei/Desktop/Summer 2019/Algorithms 1/python_data
 from graph import Graph, Vertex
 clock = 0
 
-def default_check_function(u, v):
+def default_terminate_function(u, v, list_of_child, visit_stack, parent):
 	return False
 
 def default_pre_function(v):
@@ -16,20 +16,43 @@ def default_pre_function(v):
 def default_post_function(v):
 	pass
 
+def dfs(graph, pre_function = default_pre_function, 
+		post_function = default_post_function,
+		terminate_function = default_terminate_function,
+		view_stack = False):
+	parent = {}
+	terminate = False
+	for v in graph.get_vertexes():
+		if v not in parent:
+			parent, terminate = dfs_util(graph, v, parent, pre_function, post_function, terminate_function, view_stack)
+		if terminate:
+			break
+	return parent, terminate
+
+
 # given a graph and a source vertex, this function performs DFS starting from the source.
-def dfs(graph, source, pre_function=default_pre_function, post_function = default_post_function, view_stack=False):
-	visited = {source}
-	parent = {source: None}
+def dfs_util(graph, source, parent,
+		pre_function = default_pre_function, 
+		post_function = default_post_function, 
+		terminate_function = default_terminate_function,
+		view_stack = False):
+	parent[source] = None
 	visit_stack = [[source]]
+	terminate = False
 	while len(visit_stack) > 0:
 		current_list_of_child = visit_stack[-1]
 		current_vertex = current_list_of_child[-1]
 		pre_function(current_vertex)
 		list_of_child = []
 		for child in graph.get_set_of_children(current_vertex):
-			if child not in visited:
-				visited.add(child)
+			if terminate_function(current_vertex, child, list_of_child, visit_stack, parent):
+				terminate = True
+				break
+			if child not in parent:
+				parent[child] = current_vertex
 				list_of_child.append(child)
+		if terminate:
+			break
 		if len(list_of_child) > 0:
 			visit_stack.append(list_of_child)
 			if view_stack:
@@ -52,8 +75,19 @@ def dfs(graph, source, pre_function=default_pre_function, post_function = defaul
 				post_function(current_vertex)
 				if view_stack:	
 					print(visit_stack)
+	return parent, terminate
 
 def check_cycle(graph):
+	def cycle_terminate_function(u, v, list_of_child, visit_stack, parent):
+		for visiting in visit_stack:
+			if visiting[-1] == v:
+				print("cycle detected")
+				return True
+	parent, terminate = dfs(graph, terminate_function = cycle_terminate_function)
+
+	return parent, terminate
+
+def check_cycle_2(graph):
 	visited = set()
 	for v in graph.get_vertexes():
 		if v not in visited:
@@ -92,8 +126,8 @@ def check_cycle_1(graph, vertex, visited = None, visit_stack=None):
 
 if __name__ == "__main__":
 	G = Graph(True)
-	G.make_graph([1, 2, 3, 4, 5, 6, 7, 8, 9], [(1, 2), (1, 3), (2, 4), (2, 5), (3,6), (3,7), (5,8), (5,9)])
-	dfs(G, 1)
+	G.make_graph([1, 2, 3, 4, 5, 6, 7, 8, 9], [(1, 2), (1, 3), (2, 4), (2, 5), (3,6), (3,7), (5,8), (5,9), (1, 9)])
+	print(check_cycle(G))
 
 
 
